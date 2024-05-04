4a061e0247ab1f0db4210715925f6bb0fac5aa26
Vítor Duarte Buchweitz <dbvitor@gmail.com>
2024-05-04 11:07:50 -0300

    stack with go

--- go/data-structures/heaps.md
+++
@@ -1,6 +0,0 @@
-
-A Heap is a complete binary tree data structure that satisfies the heap property: for every node the value of its children is less than or equal to its own value.
-
-Heaps are specially good for implementing priority queues, taking out the values with the highest priority.
-
-A heap is a data structure that can be expresses as complete tree (all the trees are full except for the lowest level)
--- go/data-structures/stack/main.go
+++ go/data-structures/stack/main.go
@@ -1,5 +1,78 @@
 package main
 
+import "fmt"
+
+//var stack []int
+//
+//func Push(stack *[]int, element ...int) {
+//
+//	*stack = append(*stack, element...)
+//
+//}
+//
+//func Pop(stack *[]int) {
+//	*stack = (*stack)[:len(*stack)-1]
+//}
+
+//func main() {
+//	Push(&stack, 1)
+//	Push(&stack, 2, 3, 4)
+//
+//	Pop(&stack)
+//
+//	fmt.Println("stack:", stack)
+//}
+
+//with structs
+
+type Stack struct {
+	values []int
+	top    int
+}
+
+func NewStack() *Stack {
+	return &Stack{
+		values: make([]int, 0),
+		top:    -1,
+	}
+}
+
+func (st *Stack) Push(elements ...int) {
+
+	st.top++
+	st.values = append(st.values, elements...)
+}
+
+func (st *Stack) Pop() int {
+
+	if st.IsEmpty() {
+		return 0
+	}
+
+	value := st.values[st.top]
+
+	st.values = st.values[:len(st.values)-1]
+
+	st.top--
+
+	return value
+
+}
+
+func (st *Stack) IsEmpty() bool {
+	return st.top == -1
+}
+
 func main() {
+
+	stack1 := NewStack()
+
+	stack1.Push(1, 2, 3, 4)
+
+	fmt.Println("stack", stack1)
+
+	stack1.Pop()
+
+	fmt.Println("stack", stack1)
 
 }
