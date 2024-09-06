87d44b8bde69d36823747c608229e183c242a231
Vítor Duarte Buchweitz <dbvitor@gmail.com>
2024-09-05 22:33:36 -0300

    float go

--- programming-languages/go/features/3 - Data Types/float/main.go
+++ programming-languages/go/features/3 - Data Types/float/main.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+/*
+The float data types are used to store positive and negative numbers with a decimal point, like 35.3, -2.34, or 3597.34987.
+The float data type has two keywords: float32 and float64.
+The float32 data type can store numbers with a decimal point that have up to 7 digits of precision,
+while the float64 data type can store numbers with a decimal point that have up to 15 digits of precision.
+*/
+func main() {
+
+	var x float64 = 1.7e+308
+	fmt.Printf("Type: %T, value: %v", x, x)
+
+}
